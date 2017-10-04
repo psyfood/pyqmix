1f11f7905c543b12eab2ff711e18883a83a455f6
Richard Höchenberger <richard.hoechenberger@gmail.com>
2017-10-04 18:14:23 +0200

    BF: Avoid circular import

--- pyqmix/error.py
+++ pyqmix/error.py
@@ -4,13 +4,13 @@
 import os
 from cffi import FFI
 
-from .bus import DLL_DIR
+import pyqmix.bus  # Absolute import here, to avoid circular import problem.
 from .headers import ERROR_HEADER
 
 
 class QmixError(object):
     def __init__(self, error_number):
-        self.dll_file = os.path.join(DLL_DIR, 'usl.dll')
+        self.dll_file = os.path.join(pyqmix.bus.DLL_DIR, 'usl.dll')
         self._ffi = FFI()
         self._ffi.cdef(ERROR_HEADER)
 
