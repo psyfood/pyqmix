35e369266385d62fde270685b174ee3e4993c1dd
Richard Höchenberger <richard.hoechenberger@gmail.com>
2018-07-09 22:38:44 +0200

    DOC: Update docstring

--- pyqmix/__init__.py
+++ pyqmix/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Gustometer based on neMESYS system.
+"""Interface for Cetoni Qmix SDK.
 """
 
 from __future__ import print_function, unicode_literals
