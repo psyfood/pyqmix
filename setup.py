b63e2f53302bc0b6bd01322981dbe9132494b402
Richard Höchenberger <richard.hoechenberger@gmail.com>
2018-08-08 08:18:35 +0200

    PKG: Move package metadata specifications to setup.cfg

--- setup.py
+++ setup.py
@@ -12,21 +12,7 @@ except ImportError:
 
 
 setup(
-    name='pyqmix',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    author=('Lorenzo Alfine <lorenzo.alfine@gmail.com>,'
-            'Richard Höchenberger <richard.hoechenberger@gmail.com>'),
-    url='https://github.com/psyfood/Gustometer',
-    packages=find_packages(),
-    license='GPL v3',
-    description='A wrapper for the Cetoni Qmix SDK.',
-    long_description=open('README.md').read(),
-    install_requires=['cffi', 'ruamel.yaml', 'appdirs'],
-    classifiers=['Intended Audience :: Science/Research',
-                 'Programming Language :: Python',
-                 'Operating System :: Microsoft :: Windows',
-                 'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3']
+    packages=find_packages()
 )
